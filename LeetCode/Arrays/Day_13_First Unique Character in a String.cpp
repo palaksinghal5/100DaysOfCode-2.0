5bea6a6d5676b3622cb93619fb547466388fe128
Palak Singhal <palak.singhal.agarwal@gmail.com>
2021-07-18 21:12:52 +0530

    Day 12

---
+++ LeetCode/Arrays/Day_13_First Unique Character in a String.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> hash;
+        int index = -1;
+        for(char ch : s){
+            if(hash.find(ch)==hash.end()){
+                hash.insert({ch, 0});
+            }
+            else{
+                hash[ch]++;
+            }
+        }
+        
+        for(int i=0;i<s.length();i++){
+            if(hash[s[i]]==0){
+                index = i;
+                break;
+            }
+                
+        }
+                    return index;
+    }
+};
